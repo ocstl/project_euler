5bada654a873fbd1d99d1d0365b03cc253c5e761
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-04-03 08:56:33 -0400

    Problem 20.

---
+++ src/bin/problem20.rs
@@ -0,0 +1,13 @@
+use num::BigUint;
+
+const INPUT: usize = 100;
+
+fn main() {
+    let answer: u32 = (1..INPUT).product::<BigUint>()
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .sum();
+
+    println!("Answer: {}", answer);
+}
