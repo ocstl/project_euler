f9589d3e3fae4c112467f11a264e6c18fe89555f
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-04-08 09:21:58 -0400

    Problem 25. Refactored Fibonacci to use the Clone trait instead of the Copy trait (for use with BigUint).

---
+++ src/bin/problem25.rs
@@ -0,0 +1,16 @@
+use num::BigUint;
+use project_euler::fibonacci::Fibonacci;
+
+const INPUT: usize = 1000;
+
+fn main() {
+    let lower_limit = &(1..INPUT)
+        .fold(BigUint::from(1_usize), |acc: BigUint, _| acc * 10_usize);
+
+    let answer = Fibonacci::<BigUint>::new()
+        .enumerate()
+        .find(|(_, x)| x >= lower_limit)
+        .unwrap();
+
+    println!("Term {} is {}", answer.0 + 1, answer.1);
+}
--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -3,12 +3,12 @@ extern crate num;
 use num::{Unsigned, Zero, One};
 
 #[derive(Default)]
-pub struct Fibonacci<T: Copy + Unsigned + Zero + One> {
+pub struct Fibonacci<T: Clone + Unsigned + Zero + One> {
     current: T,
     next: T,
 }
 
-impl<T: Copy + Unsigned + Zero + One> Fibonacci<T> {
+impl<T: Clone + Unsigned + Zero + One> Fibonacci<T> {
     pub fn new() -> Fibonacci<T> {
         Fibonacci {
             current: T::zero(),
@@ -17,14 +17,14 @@ impl<T: Copy + Unsigned + Zero + One> Fibonacci<T> {
     }
 }
 
-impl<T: Copy + Unsigned + Zero + One> Iterator for Fibonacci<T> {
+impl<T: Clone + Unsigned + Zero + One> Iterator for Fibonacci<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let new_next = self.current + self.next;
-        self.current = self.next;
+        let new_next = self.current.clone() + self.next.clone();
+        self.current = self.next.clone();
         self.next = new_next;
 
-        Some(self.current)
+        Some(self.current.clone())
     }
 }
