7f35f67e1d9bbfe50f76952b85a28c9565d56410
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-04-16 10:20:31 -0400

    Problem 33.

---
+++ src/bin/problem33.rs
@@ -0,0 +1,36 @@
+use num::Rational;
+
+fn main() {
+    /* We are dealing with two digits numerators and denominators, thus we can hard code a bit. */
+    let digit_cancelling_fraction = |numerator, denominator| -> bool {
+        let ratio = Rational::new(numerator, denominator);
+
+        /* Eliminate trivial fractions. */
+        if numerator % 10 == 0 && denominator % 10 == 0 {
+            false
+        } else if (numerator / 10) == (denominator / 10) && denominator % 10 != 0 &&
+                Rational::new(numerator % 10, denominator % 10) == ratio {
+            true
+        } else if (numerator / 10) == (denominator % 10) &&
+                Rational::new(numerator % 10, denominator / 10) == ratio {
+            true
+        } else if (numerator % 10) == (denominator / 10) && denominator % 10 != 0 &&
+                Rational::new(numerator / 10, denominator % 10) == ratio {
+            true
+        } else if (numerator % 10) == (denominator % 10) &&
+                Rational::new(numerator / 10, denominator / 10) == ratio {
+            true
+        } else {
+            false
+        }
+    };
+
+    let fractions: Vec<Rational> = (11..100).flat_map(move |denominator: isize| (10..denominator)
+        .filter(move |&numerator| digit_cancelling_fraction(numerator, denominator))
+        .map(move |numerator| Rational::new(numerator, denominator)))
+        .collect();
+
+    let answer = fractions.iter().product::<Rational>().denom().clone();
+
+    println!("Answer: {}", answer);
+}
