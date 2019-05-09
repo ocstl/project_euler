6c41a53fa9401534d8b66676e11438d65ff1d980
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-05-09 10:52:09 -0400

    Refactor of problem 4, using ReverseInteger trait.

--- src/bin/problem4.rs
+++ src/bin/problem4.rs
@@ -1,15 +1,20 @@
+use project_euler::tools::ReverseInteger;
+
+const MIN: u32 = 100;
+const MAX: u32 = 999;
+
 fn main() {
-    let answer = (100..999)
-        .flat_map(|x| (100..999).map(move |y| x * y))
-        .filter(|&x| palindrome(x))
+    // Find the largest palindrome that is the product of two 3-digit numbers.
+    let answer: u32 = (MIN..=MAX)
+        .map(|x| {
+            (MIN..=MAX)
+                .rev()
+                .map(|y| x * y)
+                .find(ReverseInteger::is_palindrome_base10)
+                .unwrap_or(0)
+        })
         .max()
-        .unwrap_or(0);
+        .expect("Did not find any palindrome product.");
 
     println!("Answer: {}", answer);
-}
-
-fn palindrome(a: u32) -> bool {
-    let temp = a.to_string();
-
-    temp.chars().zip(temp.chars().rev()).all(|(x, y)| x == y)
 }
