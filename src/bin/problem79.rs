bc4ab55cfe9b69dd3d5f6a0588fa15a6a3007b66
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-09-16 09:19:04 -0400

    Problem 79.

---
+++ src/bin/problem79.rs
@@ -0,0 +1,59 @@
+use project_euler::unsigned::UnsignedInteger;
+use std::collections::HashSet;
+use std::fs;
+
+const BASE: u32 = 10;
+const FILENAME: &str = "inputs/p079_keylog.txt";
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct KeyLog([u32; 3]);
+
+impl KeyLog {
+    fn check(&self, password: &[u32]) -> bool {
+        let mut it = password.iter();
+        self.0.iter().all(|&key| it.any(|&x| x == key))
+    }
+}
+
+impl From<&str> for KeyLog {
+    fn from(keylog: &str) -> Self {
+        let mut it = keylog.chars().map(|c| c.to_digit(10).unwrap());
+        KeyLog([it.next().unwrap(), it.next().unwrap(), it.next().unwrap()])
+    }
+}
+
+/// A common security method used for online banking is to ask the user for three random
+/// characters from a passcode. For example, if the passcode was 531278, they may ask for the
+/// 2nd, 3rd, and 5th characters; the expected reply would be: 317.
+///
+/// The text file, keylog.txt, contains fifty successful login attempts.
+///
+/// Given that the three characters are always asked for in order, analyse the file so as to
+/// determine the shortest possible secret passcode of unknown length.
+fn main() {
+    // Use a HashSet to avoid checking multiple times against the same key log.
+    let keylogs = fs::read_to_string(FILENAME)
+        .unwrap()
+        .lines()
+        .map(KeyLog::from)
+        .collect::<HashSet<KeyLog>>();
+
+    // The problem is small enough to lends itself to brute force.
+    let answer = (100..)
+        .find(|password| {
+            let password = password.to_radix_be(BASE);
+            keylogs.iter().all(|keylog| keylog.check(&password))
+        })
+        .unwrap();
+
+    println!("The answer is: {}", answer)
+}
+
+#[test]
+fn test_key_log() {
+    let keylog = KeyLog([1, 2, 3]);
+    assert!(keylog.check(&[1, 2, 3]));
+    assert!(!keylog.check(&[2, 1, 3]));
+    assert!(keylog.check(&[1, 2, 4, 3]));
+    assert!(!keylog.check(&[2, 1, 4, 3]));
+}
