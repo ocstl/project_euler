fd100330231f5d5a3ff5df507997ce1d1a43320e
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-09-19 11:37:23 -0400

    Problem 81: re-implement as a simplified problem 82.

--- src/bin/problem81.rs
+++ src/bin/problem81.rs
@@ -5,16 +5,32 @@ use std::fs;
 const BASE: u32 = 10;
 const FILENAME: &str = "inputs/p081_matrix.txt";
 
-type Coordinates = (usize, usize);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Coordinates(usize, usize);
+
+impl Coordinates {
+    fn new(x: usize, y: usize) -> Self {
+        Coordinates(x, y)
+    }
+
+    fn right(self) -> Option<Self> {
+        Some(Coordinates(self.0.checked_add(1)?, self.1))
+    }
+
+    fn down(self) -> Option<Self> {
+        Some(Coordinates(self.0, self.1.checked_add(1)?))
+    }
+
+    fn neighbours(self) -> impl Iterator<Item = Self> {
+        self.right().into_iter().chain(self.down().into_iter())
+    }
+}
+
 type Matrix = HashMap<Coordinates, u64>;
 type CumulativePath = (Reverse<u64>, Coordinates);
 
-fn right_and_down(c: Coordinates) -> [Coordinates; 2] {
-    [(c.0 + 1, c.1), (c.0, c.1 + 1)]
-}
-
 /// Find the minimal path sum, in matrix.txt, a text file containing a 80 by 80 matrix, from the
-/// top left to the bottom right by only moving right and down.
+/// left column to the right column by only moving right, down and up.
 fn main() {
     let input = fs::read_to_string(FILENAME).expect("Missing file.");
 
@@ -23,49 +39,54 @@ fn main() {
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
-            line.split(',')
-                .enumerate()
-                .map(move |(x, n)| ((x, y), u64::from_str_radix(n, BASE).unwrap()))
+            line.split(',').enumerate().map(move |(x, n)| {
+                (
+                    Coordinates::new(x, y),
+                    u64::from_str_radix(n, BASE).unwrap(),
+                )
+            })
         })
         .collect();
 
-    // Find the bottom right coordinate.
-    let bottom_right = *matrix.keys().max().unwrap();
+    // Generate start and end points.
+    let start = *matrix.keys().min().unwrap();
+    let end = *matrix.keys().max().unwrap();
 
-    // Implement DFS (use a Reverse<u64> to `pop` the shortest path first).
-    let mut heap: BinaryHeap<CumulativePath> = BinaryHeap::new();
-    let top_left = matrix.keys().min().unwrap();
-    let initial = (Reverse(*matrix.get(top_left).unwrap()), *top_left);
-    heap.push(initial);
+    let answer = shortest_path(start, end, &matrix).unwrap();
 
-    // Keep track of more previously visited paths, so we can flush inefficient ones.
+    println!("The answer is: {}", answer);
+}
+
+// Use DFS to find the shortest path.
+fn shortest_path(start: Coordinates, end: Coordinates, matrix: &Matrix) -> Option<u64> {
+    let mut paths: BinaryHeap<CumulativePath> = BinaryHeap::new();
+    let initial = (Reverse(*matrix.get(&start).unwrap()), start);
+    paths.push(initial);
+
+    // Keep track of previously visited paths so we can filter out inefficient ones.
     let mut visited = Matrix::new();
 
-    while let Some(current) = heap.pop() {
-        let s = (current.0).0;
-        let position = current.1;
-
-        // Ignore inefficient paths.
+    while let Some((Reverse(total), position)) = paths.pop() {
+        // Filter out inefficient paths.
         if let Some(v) = visited.get(&position) {
-            if s >= *v {
+            if total >= *v {
                 continue;
             }
-        } else {
-            visited.insert(position, s);
         }
 
-        if position == bottom_right {
-            println!("The answer is: {}", s);
-            break;
+        visited.insert(position, total);
+
+        if position == end {
+            return Some(total);
         }
 
-        // Add new paths when possible.
-        let [right, bottom] = right_and_down(position);
-        if let Some(v) = matrix.get(&right) {
-            heap.push((Reverse(s + v), right));
-        }
-        if let Some(v) = matrix.get(&bottom) {
-            heap.push((Reverse(s + v), bottom));
+        // Move to the neighbouring coordinates, if possible.
+        for new_position in position.neighbours() {
+            if let Some(v) = matrix.get(&new_position) {
+                paths.push((Reverse(total + v), new_position));
+            }
         }
     }
+
+    None
 }
