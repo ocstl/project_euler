572a69fb4ed23a683438b36603dc633557c3d32e
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-10-17 09:00:17 -0400

    fibonacci.rs: eliminate the intermediate value by swapping values.
    Problem 2: Fix problem bound.

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,12 +1,19 @@
 use num::{One, Unsigned, Zero};
+use std::ops::AddAssign;
 
 #[derive(Default)]
-pub struct Fibonacci<T: Clone + Unsigned + Zero + One> {
+pub struct Fibonacci<T>
+where
+    for<'a> T: Clone + Unsigned + Zero + One + AddAssign<&'a T>,
+{
     current: T,
     next: T,
 }
 
-impl<T: Clone + Unsigned + Zero + One> Fibonacci<T> {
+impl<T> Fibonacci<T>
+where
+    for<'a> T: Clone + Unsigned + Zero + One + AddAssign<&'a T>,
+{
     pub fn new() -> Fibonacci<T> {
         Fibonacci {
             current: T::zero(),
@@ -15,13 +22,15 @@ impl<T: Clone + Unsigned + Zero + One> Fibonacci<T> {
     }
 }
 
-impl<T: Clone + Unsigned + Zero + One> Iterator for Fibonacci<T> {
+impl<T> Iterator for Fibonacci<T>
+where
+    for<'a> T: Clone + Unsigned + Zero + One + AddAssign<&'a T>,
+{
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let new_next = self.current.clone() + self.next.clone();
-        self.current = self.next.clone();
-        self.next = new_next;
+        self.next += &self.current;
+        std::mem::swap(&mut self.current, &mut self.next);
 
         Some(self.current.clone())
     }
